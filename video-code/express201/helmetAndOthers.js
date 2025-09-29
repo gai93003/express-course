4abd67efc59a3926d78d87972fbbb33c9767a418
Gabriel <deng93003@gmail.com>
2025-09-30 00:10:06 +0100

    final changes for the night

--- video-code/express201/helmetAndOthers.js
+++ video-code/express201/helmetAndOthers.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet());
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.post('/ajax', (req, res) => {
+  console.log(req);
+  res.send("Test");
+});
