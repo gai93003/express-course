f48b6d647753c0b8f578ae089581fab4c9cd308a
Gabriel <deng93003@gmail.com>
2025-09-29 23:48:35 +0100

    more changes

--- video-code/express201/appUse.js
+++ video-code/express201/appUse.js
@@ -5,3 +5,36 @@ const app = express();
 // 1. Router
 // 2. Middleware that comprises a webframework
 
+// Req ---MIDDLEWARE---> Res
+// Middle function is ANY function that has access to the req, res, and the next Object.
+
+// 1. Request comes in.
+// 2. We need to validate the UserActivation, sometimes.
+// 3. We need to store some things in the DB.
+// 4. If there is data from the user we need to parse it and store it.
+// 5. Res
+
+function validateUser(req, res, next) {
+  // get info out of the req object
+  // do some stuff with the DB
+
+  res.locals.validated = true;
+
+  console.log("VALIDATED RAN!");
+  next();
+}
+
+// This will run validateUser on ALL paths, all methods!
+app.use('/', validateUser);
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>Main Page</h1>')
+  console.log(res.locals.validated);
+});
+
+app.get('/admin', (req, res, next) => {
+  res.send('<h1>Admin Page</h1>')
+  console.log(res.locals.validated);
+});
+
+app.listen(3000);
